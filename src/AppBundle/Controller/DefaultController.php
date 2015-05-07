fccd79850bb180f16385de78ad92888cd7d03de2
danm94 <micdan1994@gmail.com>
2015-05-07 10:31:59 +0300

    Generated entity Product,update schema and created createAction.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Product;
+use Symfony\Component\BrowserKit\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +15,20 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+    //This creates a new entry in the data base
+    public function createAction()
+    {
+        $product = new Product();
+        $product ->setName('T-Shirt');
+        $product ->setPrice('30');
+        $product ->setDescription('Beautiful Deadpool Shirt');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Creted product id '.$product->getId());
     }
 }
