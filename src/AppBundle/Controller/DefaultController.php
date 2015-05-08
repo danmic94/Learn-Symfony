8af1f5c709516c23cf5ed781263e6c146020ef2d
danm94 <micdan1994@gmail.com>
2015-05-08 14:14:11 +0300

    Added my first validation on  from Author Entity ;)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Product;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Author;
 
 
 class DefaultController extends Controller
@@ -23,9 +24,9 @@ class DefaultController extends Controller
     public function createAction()
     {
         $product = new Product();
-        $product ->setName('T-Shirt');
-        $product ->setPrice('30');
-        $product ->setDescription('Beautiful Deadpool Shirt');
+        $product->setName('T-Shirt');
+        $product->setPrice('30');
+        $product->setDescription('Beautiful Deadpool Shirt');
 
         $em = $this->getDoctrine()->getManager();
 
@@ -34,89 +35,109 @@ class DefaultController extends Controller
 
 
         //returns response that the query created new entry in db
-        return new Response('Created product id' .$product->getId());
+        return new Response('Created product id' . $product->getId());
     }
-        public function showAction($id)
-        {
-            $product = $this->getDoctrine()
-                ->getRepository('AppBundle:Product')
-                ->findOneByIdJoinedToCategory($id);
 
-            $category = $product->getCategory();
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->findOneByIdJoinedToCategory($id);
 
-            if (isset($product) === FALSE) {
-                return $this->render('dbactions/error.html.twig');
-            }
+        $category = $product->getCategory();
 
-                return $this->render('dbactions/index.html.twig', array(
-                    'product' => $product,
-                ));
+        if (isset($product) === FALSE) {
+            return $this->render('dbactions/error.html.twig');
         }
 
-        public function updateAction($id)
-        {
-            $em = $this->getDoctrine()->getManager();
-            $product = $em->getRepository('AppBundleProduct')->find($id);
+        return $this->render('dbactions/index.html.twig', array(
+            'product' => $product,
+        ));
+    }
 
-            if(!$product) {
-                throw $this->createNotFoundException(
-                    'No product found for id '.$id
-                );
-            }
-            $product->setName('New product name!');
-            $em->flush();
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('AppBundleProduct')->find($id);
 
-            return $this->redirectToRoute('homepage');
-        }
-
-        public function deleteAction($id)
-        {
-            $em = $this->getDoctrine()->getManager();
-            $product = $em->getRepository('AppBundleProduct')->find($id);
-
-            if(!$product) {
-                throw $this->createNotFoundException(
-                    'No product found for id '.$id
-                );
-            }
-            $product->remove($product);
-            $em->flush();
-
-            return $this->render('homepage');
-        }
-
-        //Generateing a row in th Database
-        public function createProductAction()
-        {
-            $category = new Category();
-            $category->setName('Main Products');
-
-            $product = new Product();
-            $product->setName('Foo');
-            $product->setPrice(21);
-            $product->setDescription('Lorem ipsum dolor');
-            //relate this product to the category
-            $product->setCategory($category);
-            $product->setCreatedAt(new \DateTime('now','UTC'));
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->persist($product);
-            $em->flush();
-
-            return new Response(
-                    'Created product id: '.$product->getId()
-                    .' and category id: '.$category->getId()
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
             );
         }
-        public function createdAtAction()
-        {
-            $product= new Product();
-            $product->setCreatedAtValue();
-            $response=$product->getCreatedAtValue();
+        $product->setName('New product name!');
+        $em->flush();
 
-            return new Response('Produsul este: '.$response);
+        return $this->redirectToRoute('homepage');
+    }
+
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('AppBundleProduct')->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
         }
+        $product->remove($product);
+        $em->flush();
+
+        return $this->render('homepage');
+    }
+
+    //Generateing a row in th Database
+    public function createProductAction()
+    {
+        $category = new Category();
+        $category->setName('Main Products');
+
+        $product = new Product();
+        $product->setName('Foo');
+        $product->setPrice(21);
+        $product->setDescription('Lorem ipsum dolor');
+        //relate this product to the category
+        $product->setCategory($category);
+        $product->setCreatedAt(new \DateTime('now', 'UTC'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($category);
+        $em->persist($product);
+        $em->flush();
+
+        return new Response(
+            'Created product id: ' . $product->getId()
+            . ' and category id: ' . $category->getId()
+        );
+    }
+
+    public function createdAtAction()
+    {
+        $product = new Product();
+        $product->setCreatedAtValue();
+        $response = $product->getCreatedAtValue();
+
+        return new Response('Produsul este: ' . $response);
+    }
+
+    public function authorAction()
+    {
+
+        $author = new Author();
+        $name= "Vasil";
+        $validator = $this->get('validator');
+        $errors = $validator->validate($author);
+
+        if (count($errors) > 0) {
+            return $this->render('author/validation.html.twig', array(
+                'errors' => $errors,
+            ));
+        }
+
+        return new Response('The author is valid! Yes!: <h1>'.$name.'</h1>');
+
+    }
 }
 
 
---
+++ src/AppBundle/Entity/Author.php
@@ -0,0 +1,19 @@
+<?php
+// src/AppBundle/Entity/Author.php
+namespace AppBundle\Entity;
+// src/AppBundle/Entity/Author.php
+// other bundles that are used
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+class Author
+{
+
+
+    public $name;
+
+
+
+
+}
